fde7a717ef4fd30755eaf97be5646199729b182d
Myroslava <milrusysurlim@gmail.com>
2025-02-24 16:49:02 +0200

    initial

---
+++ src/main/java/org/example/Bank.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Arrays;
+
+class Bank {
+    public static final int NTEST = 10000;
+    private final int[] accounts;
+    private long ntransacts;
+
+    public Bank(int n, int initialBalance){
+        accounts = new int[n];
+        Arrays.fill(accounts, initialBalance);
+        ntransacts = 0;
+    }
+
+    public void transfer(int from, int to, int amount) {
+        accounts[from] -= amount;
+        accounts[to] += amount;
+        ntransacts++;
+        if (ntransacts % NTEST == 0)
+            test();
+    }
+
+    public void test(){
+        int sum = 0;
+        for (int account : accounts) sum += account;
+        System.out.println("Transactions:" + ntransacts
+                + " Sum: " + sum);
+    }
+
+    public int size(){
+        return accounts.length;
+    }
+}
